1d0532ee1b9604da8fd058f4f208418d3fa0eb17
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-03-24 15:39:01 -0700

    Set up initial navigation:
    - Embed CollectionsTableViewController in navigation controller
    - Add CollectionsDetailTableViewController for product details
    - Update title for custom collections list page and back title

---
+++ ShopifyCollections/CollectionDetailsTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionDetailsTableViewController.swift
+//  ShopifyCollections
+//
+//  Created by Maribel Montejano on 3/24/19.
+//  Copyright © 2019 Maribel Montejano. All rights reserved.
+//
+
+import UIKit
+
+class CollectionDetailsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
