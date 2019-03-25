e4b277885da2a9accb4d961b229ebc6f8243fa24
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-03-24 20:46:39 -0700

    Add constants.swift file:
    Keys were hidden previously (apiKey)

---
+++ ShopifyCollections/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  ShopifyCollections
+//
+//  Created by Maribel Montejano on 3/24/19.
+//  Copyright © 2019 Maribel Montejano. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "c32313df0d0ef512ca64d5b336a0d7c6"
