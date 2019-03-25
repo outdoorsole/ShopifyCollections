49623c3bcf90ecbdcdfea81f46aab21af0283746
Maribel Montejano <outdoorsole@users.noreply.github.com>
2019-03-24 17:16:47 -0700

    Load the product details to CollectionDetailsTableViewController:
    - Create new helper method (getProductDetails()) to query for the individual product information using the product_ids
    - Create custom type for ProductDetails
    - Update the table view with the list of product names
    
    Todo: The product_ids are currently being hardcoded, need to change so that the query uses the product_ids that come back from the first data task (getProductIds() helper method)

--- ShopifyCollections/CollectionDetailsTableViewController.swift
+++ ShopifyCollections/CollectionDetailsTableViewController.swift
@@ -11,16 +11,18 @@ import UIKit
 class CollectionDetailsTableViewController: UITableViewController {
     var currentCollection: Collection?
     var products: Products?
+    var productDetails: ProductDetails?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(currentCollection ?? "")
+        getProductIds()
         getProductDetails()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let productsCount = products?.collects.count {
+        if let productsCount = productDetails?.products.count {
             return productsCount
         }
         return 0
@@ -28,23 +30,22 @@ class CollectionDetailsTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "productCell", for: indexPath)
-        
-        if let productId = products?.collects[indexPath.row].product_id {
-            cell.textLabel?.text = String(productId)
+        if let productName = productDetails?.products[indexPath.row].title {
+            cell.textLabel?.text = productName
         }
         
         return cell
     }
     
     // MARK: - Helper method
-    func getProductDetails() {
+    func getProductIds() {
         if let url = URL(string: "https://shopicruit.myshopify.com/admin/collects.json?collection_id=\(String(currentCollection!.id))&page=1&access_token=") {
             print("url: \(url)")
             
             // Use the shared URLSession singleton object to create a data task to get the contents for the url
             let task = URLSession.shared.dataTask(with: url) {
                 (data, response, error) in
-                print("in the completion handler for data task")
+                print("in the completion handler for data task for getProductIds()")
                 
                 if let error = error {
                     print("error: \(error)")
@@ -64,6 +65,40 @@ class CollectionDetailsTableViewController: UITableViewController {
                         
                         // Update the class property to the result from decoding
                         self.products = result
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func getProductDetails() {
+        if let url = URL(string: "https://shopicruit.myshopify.com/admin/products.json?ids=2759137027,2759143811&page=1&access_token=") {
+            print("url: \(url)")
+            
+            // Use the shared URLSession singleton object to create a data task to get the contents for the url
+            let task = URLSession.shared.dataTask(with: url) {
+                (data, response, error) in
+                print("in the completion handler for data task for getProductDetails()")
+                
+                if let error = error {
+                    print("error: \(error)")
+                    return
+                }
+                
+                if let data = data {
+                    print("data \(data)")
+                    
+                    // Create an instance of JSONDecoder to decode data
+                    let jsonDecoder = JSONDecoder()
+                    
+                    // Try to decode the returned data with the Codable ProductDetails type
+                    if let result = try? jsonDecoder.decode(ProductDetails.self, from: data) {
+                        print("in results")
+                        print(result)
+                        
+                        // Update the class property to the result from decoding
+                        self.productDetails = result
                         DispatchQueue.main.async {
                             self.tableView.reloadData()
                         }
---
+++ ShopifyCollections/ProductDetails.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetails.swift
+//  ShopifyCollections
+//
+//  Created by Maribel Montejano on 3/24/19.
+//  Copyright © 2019 Maribel Montejano. All rights reserved.
+//
+
+import Foundation
+
+struct ProductDetails: Codable {
+    let products: [ProductDetail]
+}
+
+struct ProductDetail: Codable {
+    let id: Int
+    let title: String
+}
